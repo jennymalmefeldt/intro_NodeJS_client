afc7727f00bdedefde4f0c83e22f712aaa3a6bd5
jenny_malmefeldt <jennymalmefeldt@gmail.com>
2020-10-28 17:12:15 +0100

    Creating submitHandler, addign form to create book

--- js/app.js
+++ js/app.js
@@ -1,17 +1,34 @@
 const apiUrl = "http://localhost:3002/books";
 
 const displayBooks = async () => {
-  const displayElement = document.getElementById('display')
-  const data = await ( await fetch(apiUrl)).json();
-  debugger
+  const displayElement = document.getElementById("display");
+  const data = await (await fetch(apiUrl)).json();
+
   data.books.forEach((book) => {
     const displayBookElement = document.createElement("div");
     let html = `<p>The book ${book.title}, is written by ${book.author}.`;
     displayBookElement.innerHTML = html;
-    displayElement.appendChild(displayBookElement)
+    displayElement.appendChild(displayBookElement);
+  });
+};
+
+const submitHandler = async () => {
+  event.preventDefault();
+  let author = event.target.author.value;
+  let title = event.target.title.value;
+  const response = await fetch(apiUrl, {
+    method: "POST",
+    headers: {
+      "Accept": "application/json",
+      "Content-type": "application/json",
+    },
+    body: JSON.stringify({ title: title, author: author }),
   });
 };
 
 document.addEventListener("DOMContentLoaded", () => {
   displayBooks();
+
+  const createBookForm = document.getElementById("create-book");
+  createBookForm.addEventListener("submit", submitHandler);
 });
